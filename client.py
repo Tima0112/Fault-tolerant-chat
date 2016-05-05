837121362440ed08c5e5c3f9b33236228aa42bcb
Tima <temirlan-nsk@mail.ru>
2016-05-05 20:53:21 +0600

    Почти готово

--- client.py
+++ client.py
@@ -3,8 +3,57 @@ import select
 import sys
 
 
+def getServerAddrs(filename):
+    addrServers = []
+    fd = open(filename, 'r')
+    for line in fd:
+        addrServers.append((line.split(':')[0], int(line.split(':')[1])))
+    fd.close()
+    return addrServers
+
+def try_connect_to_server(sock, addrServers):
+    for addr in addrServers:
+        flg = sock.connect_ex(addr)
+        if flg == 0:
+            return 0
+    return 1
+
+
+
 def chat_client():
-    print('Input name')
-    name = sys.stdin.readline() + ': '
+    addrServers = []
+    sizeBuf = 4096
+
+    print('Input name:')
+    name = sys.stdin.readline()[:-1] + ': '
     sock = socket.socket()
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    addrServers = getServerAddrs('server_list')
+    if try_connect_to_server(sock, addrServers):
+        print('Not available server')
+        return 1
+    epoll = select.epoll()
+    epoll.register(sock.fileno(), select.EPOLLIN)
+    epoll.register(sys.stdin.fileno(), select.EPOLLIN)
+
+    while 1:
+        pairs = epoll.poll()
+        for fileno, event in pairs:
+            if event & select.EPOLLIN:
+                if fileno == sock.fileno():
+                    msg = sock.recv(sizeBuf)
+                    if msg:
+                        msg = msg.decode()
+                        print(msg)
+                    else:
+                        print(sock.getsockname())
+                        if try_connect_to_server(sock, addrServers):
+                            print('Not available server')
+                            return 1
+                elif fileno == sys.stdin.fileno():
+                    msg = name + sys.stdin.readline()
+                    sock.send(msg.encode())
+                    print(msg)
+
+if __name__ == "__main__":
+    chat_client()
--- server.py
+++ server.py
@@ -2,112 +2,250 @@ import sys
 import socket
 import select
 import pdb
+import pickle
+import copy
+
+class Message:
+    types = ['msg', 'tree']
+    type = ''
+
+    def __init__(self, type, msg=None, tree=None):
+        index = self.types.index(type)
+        self.type = self.types[index]
+        if index == 0:
+            self.msg = copy.copy(msg)
+        elif index == 1:
+            self.tree = copy.deepcopy(tree)
 
 
 
-def try_bind(addrServers):
-    sock = socket.socket()
-    flg = 1
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    fd = open('server_list', 'r')
-    i = 0
-    for line in fd:
-        ipPort = (line.split(':')[0], int(line.split(':')[1]))
-        if flg:
-            try:
-                sock.bind(ipPort)
-                sock.listen(10)
-                flg = 0
-                index = i
-            except socket.error:
-                addrServers.append(ipPort)
-        else:
-            addrServers.append(ipPort)
-        i += 1
 
-    fd.close()
-    if flg == 1:
-        return None, index
-    else:
-        return sock, index
-
-
-def try_connect_server(addrServers, myindex):
-    sock = socket.socket()
-    sock.bind((addrServers[myindex][0], addrServers[myindex][1] + 1))
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    for addr in addrServers:
-        flg = sock.connect_ex(addr)
-        if flg == 0:
-            return sock
-    return None
-
-
-def send_hist(sock, fd):
-    fd.seek(0, 0)
-    buf = fd.read()
-    sock.send(buf)
-
-
-def broadcast(socketsClient, msg):
-    for sock in socketsClient:
-        try:
-            sock.send(msg)
-        except:
-            sock.close()
-            if sock in socketsClient:
-                socketsClient.pop(sock.fileno())
-
-
-def chat_server():
-    socketsClient = {}
+class Chat_server:
     addrServers = []
-    sizeBuf = 1024
-    # pdb.set_trace()
-    serversock, myindex = try_bind(addrServers)
-    if serversock == None:
-        print('All addresses is busy')
-        return
-    fdw = open('hist' + str(myindex), 'a')
-    fdr = open('hist' + str(myindex), 'r')
-    epoll = select.epoll()
-    epoll.register(serversock.fileno(), select.EPOLLIN)
-    otherServer = try_connect_server(addrServers, myindex)
-    if otherServer != None:
-        epoll.register(otherServer.fileno(), select.EPOLLIN)
+    socketsClient = {}
+    socketsServer = {}
+    parentSock = None
+    parentIndex = -1
+    sock = None
+    tree_conn = {}
+    sizeBuf = 4096
 
-    while 1:
-        pairs = epoll.poll()
-        for fileno, event in pairs:
-            if fileno == serversock.fileno():
-                sock, addr = serversock.accept()
-                sock.setblocking(0)
-                socketsClient[sock.fileno()] = sock
-                epoll.register(sock.fileno(), select.EPOLLIN)
-                addr = (addr[0], addr[1] - 1)
 
-                if otherServer != None and (addr not in addrServers):
-                    epoll.unregister(otherServer.fileno())
-                    otherServer = None
-                send_hist(sock, fdr)
-            elif event & select.EPOLLIN:
-                sock = socketsClient[fileno]
+    def try_bind(self, filename):
+        sock = socket.socket()
+        flg = 1
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        fd = open(filename, 'r')
+        i = 0
+        for line in fd:
+            ipPort = (line.split(':')[0], int(line.split(':')[1]))
+            if flg:
                 try:
-                    data = sock.recv(sizeBuf)
-                    if data:
-                        broadcast(socketsClient, data)
-                        fdw.write(data)
-                        fdw.flush()
+                    sock.bind(ipPort)
+                    sock.listen(10)
+                    flg = 0
+                    self.index = i
+                    print('bind (%s, %d)' % ipPort)
+                except socket.error:
+                    pass
+            self.addrServers.append(ipPort)
+            i += 1
+
+        fd.close()
+        if not flg:
+            self.sock = sock
+            self.tree_conn[self.index] = []
+        else:
+            sock.close()
+            raise Exception('All addresses is busy')
+
+    def try_connect_parent(self):
+        addrServers = self.addrServers
+        if self.parentSock == None:
+            sock = socket.socket()
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((addrServers[self.index][0], addrServers[self.index][1] + 1))
+            size = len(addrServers)
+            curindex = self.index
+            for i in range(size - 1):
+                curindex = [curindex - 1, size - 1][curindex - 1 < 0]
+                flg = sock.connect_ex(addrServers[curindex])
+                if flg == 0:
+                    self.parentSock = sock
+                    self.parentIndex = curindex
+                    return 0
+            return 1
+        else:
+            parent = self.parentIndex
+            rval = self.tree_conn[parent].index(self.index)
+            while 1:
+                for i in range(rval):
+                    flg = self.parentSock.connect_ex(addrServers[self.tree_conn[parent][0]])
+                    if flg == 0 and self.index != self.tree_conn[parent][0]:
+                        self.parentIndex = self.tree_conn[parent][0]
+                        return 0
                     else:
-                        if sock in socketsClient:
-                            socketsClient.pop(sock.fileno())
-                            print("Client (%s, %s) is offline" % addr)
-                except:
-                    if sock in socketsClient:
-                        socketsClient.pop(sock.fileno())
-                        print("Client (%s, %s) is offline" % addr)
+                        self.tree_conn[parent].pop(0)
+                flg = 0
+                for key, childs in self.tree_conn.items():
+                    if parent in childs:
+                        #self.childs.remove(parent)
+                        parent = key
+                        rval = len(childs)
+                        flg = 1
+                        break
+                if flg == 0:
+                    return 1
+
+
+
+
+    def send_allhist(self, sock, fd):
+        fd.seek(0, 0)
+        buf = fd.read()
+        if sock.fileno() in self.socketsServer.keys():
+            msg = Message('msg', msg=buf)
+            msg = pickle.dumps(msg)
+            sock.send(msg)
+        elif sock.fileno() in self.socketsClient.keys():
+            sock.send(buf.encode())
+
+    # def send_tree_conn(self, sock):
+    #     msg = Message('tree', msg=self.tree_conn)
+    #     msg = pickle.dumps(msg)
+    #     sock.send(msg)
+
+
+    def broadcast(self, msg, except_sock, sockets):
+        for fileno, sock in sockets.items():
+            index = -1
+            if sockets == self.socketsServer:
+                sock, index = sock
+            if sock == except_sock:
+                continue
+            try:
+                sock.send(msg)
+            except socket.error:
+                if index != -1:
+                    self.tree_conn[self.index].remove(index)
+                self.sockets.pop(fileno)
+                sock.close()
+
+    def __init__(self, filename):
+        self.try_bind(filename)
+
+    def accept_conn(self, epoll, fdr):
+        sock, addr = self.sock.accept()
+        # sock.setblocking(0)
+        #self.socketsClient[sock.fileno()] = sock
+        epoll.register(sock.fileno(), select.EPOLLIN)
+        caddr = (addr[0], addr[1] - 1)
+        if caddr in self.addrServers:
+            index = self.addrServers.index(caddr)
+            self.socketsServer[sock.fileno()] = (sock, index)
+            self.tree_conn[self.index].append(self.addrServers.index(caddr))
+            msg = pickle.dumps(Message('tree', tree=self.tree_conn))
+            self.broadcast(msg, self.parentSock, self.socketsServer)
+            print('connect Server №%d\n' % index)
+            print(self.tree_conn, end='\n\n')
+        else:
+            self.socketsClient[sock.fileno()] = sock
+            print('connect Client (%s, %d)' % sock.getsockname())
+        self.send_allhist(sock, fdr)
+
+    def disconnect(self, sock, flg, fdw, epoll):
+        if flg == 0:
+            val = self.socketsClient.pop(sock.fileno())
+            epoll.unregister(sock.fileno())
+            print("Client (%s, %d) is offline" % val.getsockname())
+        elif flg == 1:
+            val = self.socketsServer.pop(sock.fileno())
+            epoll.unregister(sock.fileno())
+            print("Server (%s, %d) is offline" % val[0].getsockname())
+            if val[0] == self.parentSock:
+                if self.try_connect_parent() == 0:
+                    fdw.close()
+                    fdw = open('hist' + str(self.index), 'w')
+            else:
+                self.tree_conn[self.index].remove(val[1])
+            data = pickle.dumps(Message('tree', tree=self.tree_conn))
+            self.broadcast(data, self.parentSock, self.socketsServer)
+
+    def run(self):
+        sizeBuf = 1024
+        socketsClient = self.socketsClient
+        socketsServer = self.socketsServer
+
+        fdw = open('hist' + str(self.index), 'w')
+        fdr = open('hist' + str(self.index), 'r')
+        epoll = select.epoll()
+        epoll.register(self.sock.fileno(), select.EPOLLIN)
+
+        if self.try_connect_parent() == 0:
+            epoll.register(self.parentSock.fileno(), select.EPOLLIN)
+            socketsServer[self.parentSock.fileno()] = (self.parentSock, self.parentIndex)
+
+        while 1:
+            pairs = epoll.poll()
+            for fileno, event in pairs:
+                if fileno == self.sock.fileno():
+                    self.accept_conn(epoll, fdr)
+
+                elif event & select.EPOLLIN:
+                    flg = -1
+                    except_sock = None
+
+                    if fileno in socketsClient.keys():
+                        sock = socketsClient[fileno]
+                        print('Recv msg from Client (%s, %d)', sock.getpeername())
+                        flg = 0
+                    elif fileno in socketsServer.keys():
+                        sock, index = socketsServer[fileno]
+                        except_sock = sock
+                        print('Recv msg from Server (%s, %d)', sock.getpeername())
+                        flg = 1
+                    try:
+                        data = sock.recv(sizeBuf)
+                        if data:
+                            if flg == 0:
+                                msg = Message('msg', msg=data.decode())
+                                data = pickle.dumps(msg)
+                            elif flg == 1:
+                                msg = pickle.loads(data)
+                            else: continue
+                            if msg.type == 'msg':
+                                self.broadcast(msg.msg.encode(), sock, socketsClient)
+                                self.broadcast(data, except_sock, socketsServer)
+                                fdw.write(msg.msg)
+                                fdw.flush()
+                            elif msg.type == 'tree':
+                                self.tree_conn.update(msg.tree)
+                                data = pickle.dumps(Message('tree', tree=self.tree_conn))
+                                self.broadcast(data, except_sock, socketsServer)
+                                print(self.tree_conn, '\n')
+
+                        else:
+                            print('NO DATA')
+                            self.disconnect(sock, flg, fdw, epoll)
+                            print(self.tree_conn, '\n')
+
+
+                    except socket.error:
+                        print('except socket.error #1')
+
+                elif event & select.EPOLLHUP:
+                    print('event & select.EPOLLHUP')
+                    if fileno in socketsClient.keys():
+                        sock = socketsClient[fileno]
+                        flg = 0
+                    elif fileno in socketsServer.keys():
+                        sock, index = socketsServer[fileno]
+                        flg = 1
+                    else: continue
+                    self.disconnect(sock, flg, fdw, epoll)
+                    print(self.tree_conn, '\n')
 
 
 if __name__ == '__main__':
-    chat_server()
-
+    chat = Chat_server('server_list')
+    chat.run()
